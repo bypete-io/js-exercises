a487761ad26a6afbd84989610059f06ec7ea4d09
bypete-io <dev@bypete.uk>
2024-06-24 17:46:08 +0100

    fix: Apply fix for exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0 ) return 'ERROR';
+    let phrase = "";
+    for (let index=0; index < num; index += 1){
+        phrase += string;
+    }
+    return phrase;
 };
 
 // Do not edit below this line
