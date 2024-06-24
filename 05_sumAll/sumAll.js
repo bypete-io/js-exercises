b4947fafc3750fbf13e464041db19d0b0d068e4a
bypete-io <dev@bypete.uk>
2024-06-24 20:11:50 +0100

    fix: Apply fix for exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (min < 0 || max < 0){
+        return 'ERROR'
+    }
+    if (typeof min !== 'number' || typeof max !== 'number'){
+        return 'ERROR'
+    }
+    
+    let range = [min, max];
+    
+    range.sort();
+    let sum = 0;
 
+    for (let index = range[0]; index <= range[1]; index++) {
+        sum += index;
+    }
+    return sum;
 };
 
 // Do not edit below this line
