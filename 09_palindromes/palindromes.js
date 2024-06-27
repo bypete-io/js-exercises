74c0963815508e5d6afc7d2d5318deb94a1a6014
bypete-io <dev@bypete.uk>
2024-06-27 17:23:29 +0100

    fix: Apply fix for exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanString = string
+        .toLowerCase()    
+        .split('')
+        .filter((char) => alphanumerical.includes(char))
+        .join('');
 
+    const reversed = cleanString.split('').reverse().join('');
+    return cleanString === reversed;
 };
 
 // Do not edit below this line
