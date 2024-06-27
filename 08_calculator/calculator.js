44426d7a1f89b7e21ac0fcd4f79c6d11c83ebf89
bypete-io <dev@bypete.uk>
2024-06-27 15:53:55 +0100

    fix: Apply fix for exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a+b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a-b;	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const total = array.reduce((acc, cur) => acc + cur, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const total = array.reduce((acc, cur) => acc * cur, 1);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 1){
+    return 1;
+  }
+	let total = 1;
+  for (let index = n; index > 0; index--) {
+    total *= index 
+  }
+  return total
 };
 
 // Do not edit below this line
