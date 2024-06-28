7528f6e82886dd57fd0d84d6e8e2aecf8c4da07f
bypete-io <dev@bypete.uk>
2024-06-28 13:38:22 +0100

    fix: Apply fix for exercise 11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    let fibPrev = 0;
+    let fibStart = 1;
 
+    let count;
+
+    if (typeof n !== 'number') {
+        count = parseInt(n);
+    } else {
+        count = n;
+    }
+    if (count < 0) {
+        return 'OOPS';
+    }
+
+    if (count == 0) return 0;
+
+    for (let i = 2; i <= count; i++) {
+        let current = fibPrev + fibStart; // 0 + 1
+        fibPrev = fibStart; // 1
+        fibStart = current;
+    }
+    return fibStart;
 };
 
 // Do not edit below this line
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    return books.map((book) => book.title)
 
+    // Reduce method: 
+    // return books.reduce((acc, cur) => {
+    //     acc.push(cur.title);
+    //     return acc;
+    // }, [])
 };
 
 // Do not edit below this line
